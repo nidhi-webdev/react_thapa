4fbd3dc33c1ed8a9becbc16050204c6a6a2cff5b
nidhi <kumarinidhi0594@gmail.com>
2026-01-16 21:44:51 +0100

    Dynamic value

--- project/src/Cards.jsx
+++ project/src/Cards.jsx
@@ -1,6 +1,9 @@
 
 const Cards = () => {
     const name = "Queen of Tears example"
+    const  ratings = 8.2
+    const summary = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
 
   return (
     <div>
@@ -8,9 +11,8 @@ const Cards = () => {
         <img src='https://images3.alphacoders.com/130/thumb-1920-1306841.jpg' className='w-[40%] h-[40%]' />
       </div>
       <h2> Name: {name} </h2>
-      <h3> Ratings:8.2 </h3>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      <h3> Ratings: {ratings} </h3>
+      <p> {summary}
       </p>
     </div>
   )
