ac1a64034b1e1ca178cfbe1869168eb643cc4dbb
nidhi <kumarinidhi0594@gmail.com>
2026-01-17 11:58:29 +0100

    Function call

--- project/src/Cards.jsx
+++ project/src/Cards.jsx
@@ -4,6 +4,10 @@ const Cards = () => {
     const  ratings = 8.2
     const summary = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
+   const returnGerne = () => {
+    const genre = "Krimi"
+    return genre
+   }
 
   return (
     <div>
@@ -12,8 +16,8 @@ const Cards = () => {
       </div>
       <h2> Name: {name} </h2>
       <h3> Ratings: {ratings} </h3>
-      <p> {summary}
-      </p>
+      <p> {summary}</p>
+      <p>Gerne: {returnGerne()} </p>
     </div>
   )
 }
