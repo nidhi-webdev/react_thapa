44ffda241cdecdb6a4b5f7fcba003c24c60f1847
nidhi <kumarinidhi0594@gmail.com>
2026-01-17 13:54:58 +0100

    Map method in cards

--- project/src/Cards.jsx
+++ project/src/Cards.jsx
@@ -3,23 +3,27 @@ import Data from './api/Data'
 
 
 const Cards = () => {
-
-
     return (
-        <div className="m-4">
-            <div>
-                <img src={Data[0].img} className='w-[40%] h-[40%]' />
-            </div>
-            <h2> Name: {Data[0].name} </h2>
-            <h3> Ratings: {Data[0].rating} </h3>
-            <p> Description: {Data[0].description}</p>
-            <p>Gerne: {Data[0].genre} </p>
-            <p> Cast: {Data[0].cast} </p>
-            <a href={Data[0].watch_url} target='_blank'> 
-            <button className="bg-emerald-600 px-4 py-2 rounded"> Watch Now </button>
-            </a>
-        </div>
-    )
+        <>
+            {Data.map((elem, idx) => {
+                return (
+                    <div className="m-4" key={idx}>
+                        <div>
+                            <img src={elem.img} className='w-[40%] h-[40%]' />
+                        </div>
+                        <h2> Name: {elem.name} </h2>
+                        <h3> Ratings: {elem.rating} </h3>
+                        <p> Description: {elem.description}</p>
+                        <p>Gerne: {elem.genre} </p>
+                        <p> Cast: {elem.cast ? elem.cast.join(', ') : ''} </p>
+                        <a href={elem.watch_url} target='_blank' rel="noopener noreferrer"> 
+                            <button className="bg-emerald-600 px-4 py-2 rounded"> Watch Now </button>
+                        </a>
+                    </div>
+                )
+            })}
+        </>
+    );
 }
 
 export default Cards
