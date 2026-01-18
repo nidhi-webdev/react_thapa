32a4627a6849e45dcfd546a476dd5f3840a6f63d
nidhi <kumarinidhi0594@gmail.com>
2026-01-18 12:56:09 +0100

    Solving a challenge

---
+++ project/src/ProfileCards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProfileCards = () => {
+  return (
+    <div>
+      <h2> Name: </h2>
+      <p> Age: </p>
+      <p> {} </p>
+      <div></div>
+    </div>
+  )
+}
+
+export default ProfileCards
