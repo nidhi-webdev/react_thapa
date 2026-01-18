9568683566aa977078f046cf1d14a4b839e59757
nidhi <kumarinidhi0594@gmail.com>
2026-01-18 12:42:00 +0100

    More destructering

--- project/src/CardsItem.jsx
+++ project/src/CardsItem.jsx
@@ -4,20 +4,20 @@ const CardsItem = (props) => {
     console.log("From CardItem", props);
 
 
-    const { data } = props;
+    const { img, name, rating, description, genre, cast, watch_url  } = props.data;
 
     return (
 
         <div className="m-4">
             <div>
-                <img src={data.img} className='w-[40%] h-[40%]' alt={data.name} />
+                <img src={img} className='w-[40%] h-[40%]' alt={name} />
             </div>
-            <h2> Name: {data.name} </h2>
-            <h3> Ratings: {data.rating} </h3>
-            <p> Description: {data.description}</p>
-            <p>Gerne: {data.genre} </p>
-            <p> Cast: {data.cast ? data.cast.join(', ') : ''} </p>
-            <a href={data.watch_url} target='_blank' rel="noopener noreferrer">
+            <h2> Name: {name} </h2>
+            <h3> Ratings: {rating} </h3>
+            <p> Description: {description}</p>
+            <p>Gerne: {genre} </p>
+            <p> Cast: {cast ? cast.join(', ') : ''} </p>
+            <a href={watch_url} target='_blank' rel="noopener noreferrer">
                 <button className="bg-emerald-600 px-4 py-2 rounded"> Watch Now </button>
             </a>
         </div>
