c736e6d69ef7b8718306d6fce5eabbf48a3a1f55
nidhi <kumarinidhi0594@gmail.com>
2026-01-20 18:06:35 +0100

    tailwind desing1

--- project/src/Cards.jsx
+++ project/src/Cards.jsx
@@ -3,7 +3,7 @@ import CardsItem from './CardsItem'
 
 const Cards = () => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
+        <div className="grid grid-cols-1 gap-6 p-6">
             {Data.map((item, idx) => (
                 <CardsItem key={idx} data={item} />
             ))}
