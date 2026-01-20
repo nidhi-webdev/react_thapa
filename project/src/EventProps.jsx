5a40c1fa3c479c189c25fe1fd2d85f7a21e7cd36
nidhi <kumarinidhi0594@gmail.com>
2026-01-20 18:15:50 +0100

    Define the function

--- project/src/EventProps.jsx
+++ project/src/EventProps.jsx
@@ -1,10 +1,16 @@
 export const EventProps = () => {
-    
-    
-    
+
+    const HandleWelcomeUser = (user) => {
+        alert(`Hey ${user}`)
+    }
+
+    const handleHover = () => {
+        alert("Thanks for Hovering me ")
+    }
+
     return (<>
         <WelcomeUser onclick={() => HandleWelcomeUser("Nidhi")}
-        onMouseEnter = {handleHover}
+            onMouseEnter={handleHover}
         />
     </>)
 }
