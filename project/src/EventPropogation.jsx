82056c8eda632130cf65a0fe6036bb941fea5653
nidhi <kumarinidhi0594@gmail.com>
2026-01-21 21:01:55 +0100

    Defined Function

--- project/src/EventPropogation.jsx
+++ project/src/EventPropogation.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
 
 const EventPropogation = () => {
+
+ const handleGrandParentClick = () => {
+    console.log("GrandParent Clicked"); 
+ }
+
+ const handleParentClick = () => {
+    console.log("Parent Clicked"); 
+ }
+
+ const handleChildClick = (e) => {
+    console.log(e); 
+    console.log("Child clicked")
+ }
+
   return (
     <section>
         <div onClick={handleGrandParentClick}>
