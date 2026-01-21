3c34d15468fe87cef524ceec9bdcb4edf1a646e8
nidhi <kumarinidhi0594@gmail.com>
2026-01-21 21:43:29 +0100

    event Captureing

--- project/src/EventPropogation.jsx
+++ project/src/EventPropogation.jsx
@@ -19,9 +19,9 @@ const EventPropogation = () => {
 
   return (
     <section className='flex items-center justify-center min-h-screen'>
-      <div className='bg-sky-400 border-4 border-black flex items-center justify-center h-125 w-225' onClick={handleGrandParentClick}>
-        <div className='bg-emerald-400 border-4 border-black flex items-center justify-center h-75 w-125' onClick={handleParentClick}>
-          <button className='bg-amber-400 px-4 py-2 rounded  border-4 border-black flex items-center justify-center h-20 w-75 ' onClick={handleChildClick}> Child Div </button>
+      <div className='bg-sky-400 border-4 border-black flex items-center justify-center h-125 w-225' onClickCapture={handleGrandParentClick}>
+        <div className='bg-emerald-400 border-4 border-black flex items-center justify-center h-75 w-125' onClickCapture={handleParentClick}>
+          <button className='bg-amber-400 px-4 py-2 rounded  border-4 border-black flex items-center justify-center h-20 w-75 ' onClickCapture={handleChildClick}> Child Div </button>
         </div>
       </div>
     </section>
