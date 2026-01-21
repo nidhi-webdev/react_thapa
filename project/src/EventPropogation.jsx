6c337d7569ccca6d81541a9952f42f760cbb9dad
nidhi <kumarinidhi0594@gmail.com>
2026-01-21 21:42:02 +0100

    Stop Propogation1

--- project/src/EventPropogation.jsx
+++ project/src/EventPropogation.jsx
@@ -2,27 +2,28 @@ import React from 'react'
 
 const EventPropogation = () => {
 
- const handleGrandParentClick = () => {
-    console.log("GrandParent Clicked"); 
- }
+  const handleGrandParentClick = () => {
+    console.log("GrandParent Clicked");
+  }
 
- const handleParentClick = () => {
-    console.log("Parent Clicked"); 
- }
+  const handleParentClick = (e) => {
+    e.stopPropagation()
+    console.log("Parent Clicked");
+  }
 
- const handleChildClick = (e) => {
-    console.log(e); 
+  const handleChildClick = (e) => {
+    console.log(e);
     e.stopPropagation()
     console.log("Child clicked")
- }
+  }
 
   return (
     <section className='flex items-center justify-center min-h-screen'>
-        <div className='bg-sky-400 border-4 border-black flex items-center justify-center h-125 w-225' onClick={handleGrandParentClick}>
-            <div className='bg-emerald-400 border-4 border-black flex items-center justify-center h-75 w-125' onClick={handleParentClick}>
-                <button className='bg-amber-400 px-4 py-2 rounded  border-4 border-black flex items-center justify-center h-20 w-75 ' onClick={handleChildClick}> Child Div </button>
-            </div>
+      <div className='bg-sky-400 border-4 border-black flex items-center justify-center h-125 w-225' onClick={handleGrandParentClick}>
+        <div className='bg-emerald-400 border-4 border-black flex items-center justify-center h-75 w-125' onClick={handleParentClick}>
+          <button className='bg-amber-400 px-4 py-2 rounded  border-4 border-black flex items-center justify-center h-20 w-75 ' onClick={handleChildClick}> Child Div </button>
         </div>
+      </div>
     </section>
   )
 }
