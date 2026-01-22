3cdf0073a1fbaca14bab5daaeba5a5e780379953
nidhi <kumarinidhi0594@gmail.com>
2026-01-22 20:11:08 +0100

    Usestate Increment

--- project/src/UseState.jsx
+++ project/src/UseState.jsx
@@ -2,14 +2,19 @@ import { useState } from "react"
 
 
 const UseStateComponent = () => {
-const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
+    const increment = () => {
+        setCount(count + 1)
+        
+    }
 
-  return (
-    <div>
-      <button className="bg-emerald-600 absolute left-[45%] top-[50%] px-4 py-2 rounded"> Increment </button>
-    </div>
-  )
+    return (
+        <div className="absolute left-[45%] top-[50%]">
+            <h1 className="text-3xl"> {count} </h1>
+            <button className="bg-emerald-600  px-4 py-2 rounded" onClick={increment}> Increment </button>
+        </div>
+    )
 }
 
 export default UseStateComponent
