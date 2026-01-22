f263a16deafc1079a08b58841e4611dee21f0392
nidhi <kumarinidhi0594@gmail.com>
2026-01-22 20:56:28 +0100

    Second Question

--- project/src/UseState.jsx
+++ project/src/UseState.jsx
@@ -1,18 +1,23 @@
 // Question 
-
+import { useState } from 'react'
 
 const UseState = () => {
 
-    const users = [
+
+    const [users, setUsers] = useState([
         { name: "Alice", age: 25 },
         { name: "Bob", age: 30 },
         { name: "Charlie", age: 35 },
         { name: "Angles", age: 45 },
-    ];
+    ]);
+
 
     return (
         <div>
             <h1> Users List </h1>
+
+           
+
 
             <ul>
                 {users.map((elem, idx) => {
