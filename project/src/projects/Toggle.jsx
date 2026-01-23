9a77810a1e5d88e354be2512adaaf77a3bcf0780
nidhi <kumarinidhi0594@gmail.com>
2026-01-23 18:27:42 +0100

    Some Imrovement

--- project/src/projects/Toggle.jsx
+++ project/src/projects/Toggle.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Toggle = () => {
   return (
-    
-     <div>
+    // <div className='m'>
+     <div className='bg-gray-500 p-10 w-50 m-10'>
       <div className='bg-red-600 '><span> ON </span></div>
      </div>
+    //  </div>
     
   )
 }
