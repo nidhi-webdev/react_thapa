f6fd68c6e70da7d7efeaed5c47ca31ee923d270c
nidhi <kumarinidhi0594@gmail.com>
2026-01-24 13:14:42 +0100

    Design for Toggle

--- project/src/projects/Toggle.jsx
+++ project/src/projects/Toggle.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 const Toggle = () => {
   return (
     // <div className='m'>
-     <div className='bg-gray-500 p-10 w-50 m-10'>
-      <div className='bg-red-600 '><span> ON </span></div>
-     </div>
+    <div className='bg-gray-500 p-10 w-60 m-10 rounded-full'>
+      <div className="bg-red-600 w-20 h-20 rounded-full flex items-center justify-center text-white font-bold cursor-pointer">
+        ON
+      </div>
+
+    </div>
     //  </div>
-    
+
   )
 }
 
