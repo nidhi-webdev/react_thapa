98dbe43f1c9a634efc110a1313d4dec40f0eb539
nidhi <kumarinidhi0594@gmail.com>
2026-01-24 13:51:30 +0100

    Toggle is ready

--- project/src/projects/Toggle.jsx
+++ project/src/projects/Toggle.jsx
@@ -9,9 +9,9 @@ const Toggle = () => {
   }
 
   return (
-    <div className='bg-gray-500 p-10 w-60 m-10 rounded-full' onClick={handleToggle}>
-      <div className={`bg-gray-200 w-20 h-20 rounded-full flex items-center justify-center text-white font-bold cursor-pointer ${isOn ? "on" : "off"}`}>
-       <span> {isOn ? "ON" : "OFF"} </span>
+    <div className='bg-gray-500 p-10 w-60 m-10 rounded-full' onClick={handleToggle} style={{ background: isOn ? "#4caf50" : "#f44336" }}>
+      <div className={`bg-gray-200 w-20 h-20 rounded-full flex items-center justify-center text-white font-bold cursor-pointer border-amber-100 border-2 ${isOn ? "on" : "off"}`}>
+        <span> {isOn ? "ON" : "OFF"} </span>
       </div>
     </div>
 
