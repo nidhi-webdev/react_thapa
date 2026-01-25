5077d68f83ff6fe3cb83d5c51771853d77e70af6
nidhi <kumarinidhi0594@gmail.com>
2026-01-25 13:08:39 +0100

    fire OnChange event

--- project/src/projects/Todo/ToDo.jsx
+++ project/src/projects/Todo/ToDo.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import { useState } from "react"
 
 const ToDo = () => {
+    const [inputVlaue, setInputVlaue] = useState("")
+
+    const handleInputChange = (userInputValue) => {
+        setInputVlaue(userInputValue)
+    }
+
     return (
         <div className='p-[3%] items-center flex flex-col min-h-screen bg-[linear-gradient(100deg,#001214,#001f29)] overflow-hidden transition-all duration-300 ease-linear'>
             <header className='flex flex-col items-center justify-around text-5xl min-h-[25vh]'>
@@ -12,10 +18,12 @@ const ToDo = () => {
                         type="text"
                         autoComplete="off"
                         className='bg-[#f2f3f4] p-2.5 font-[1.8rem] rounded-l-2xl rounded-r-none outline-none flex-1'
+                        onChange={(e) => handleInputChange(e.target.value)} value={inputVlaue}
                     />
                     <button
                         type="submit"
-                        className='p-2.5 text-white text-[17px] rounded-l-none rounded-r-2xl min-w-30 bg-[#5dade2]'
+                        className='p-2.5 text-white text-[17px] rounded-l-none rounded-r-2xl min-w-30 bg-[#5dade2] hover:bg-amber-300 cursor-pointer'
+
                     >
                         Add Task
                     </button>
