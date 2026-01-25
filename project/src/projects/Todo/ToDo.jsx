7750a2b6925a2f88b805e597effac6c4add404fc
nidhi <kumarinidhi0594@gmail.com>
2026-01-25 12:54:51 +0100

    Design

--- project/src/projects/Todo/ToDo.jsx
+++ project/src/projects/Todo/ToDo.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 
 const ToDo = () => {
-  return (
-   <section>
-    <header>
-        <h1> Todo List </h1>
-    </header>
-    <section>
-        <form>
-            <div> 
-                <imput type="text" autocomplete="off" />
-            </div>
+    return (
+        <div className='p-[3%] items-center flex flex-col min-h-screen bg-[linear-gradient(100deg,#001214,#001f29)] overflow-hidden transition-all duration-300 ease-linear'>
+            <header className='flex flex-col items-center justify-around text-5xl min-h-[25vh]'>
+                <h1 className='font-bold text-2xl text-white'> Todo List </h1>
+            </header>
+            <section className=' '>
+                <form className='flex font-[1.8rem]  mx-14 p-3 '>
+                    <div>
+                        <input type="text" autoComplete="off" className='bg-[linear-gradient(100deg,#f2f3f4)] p-2.5 font-[1.8rem] rounded-2xl' />
+                    </div>
 
-            <div type="submit"> Add Task </div>
-        </form>
-    </section>
-   </section>
-  )
+                    <button type="submit" className='p-2.5 text-white text-[17px] rounded-tr-[15px] rounded-br-[15px] min-w-[25 bg-[#5dade2]'> Add Task </button>
+                </form>
+            </section>
+        </div>
+    )
 }
 
 export default ToDo
