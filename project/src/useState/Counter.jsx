30fc6ce07e914213e8686d45f2da7deb4c5da879
nidhi <kumarinidhi0594@gmail.com>
2026-01-31 12:58:03 +0100

    Hooks Started

---
+++ project/src/useState/Counter.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+const Counter = () => {
+    const [first, setfirst] = useState(second)
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Counter
