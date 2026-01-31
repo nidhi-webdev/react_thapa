0bea346d260934165187a46395f30e6636dc98bd
nidhi <kumarinidhi0594@gmail.com>
2026-01-31 12:17:24 +0100

    ternary operator with condition

--- project/src/projects/shortCircuit/ShortCircuit.jsx
+++ project/src/projects/shortCircuit/ShortCircuit.jsx
@@ -16,13 +16,14 @@ const ShortCircuit = () => {
 
             <div className='ml-24'>
                 {isloggedIn && <p className='mb-5 text-xl'> You are logged in!</p>}
-               <div className="mb-2"> {user ? `Hello ${user}` : ""} </div>
+               <div className="mb-2"> {user ? `Hello ${user}` : "Please log In"} </div>
                 <button onClick={() => setIsloggedIn(!isloggedIn)}
                     className='bg-black px-36 py-2 text-white mr-3 cursor-pointer'> Toggle Login State </button>
 
                 
                 <button onClick={() => setUser("Nidhi")}
                     className='bg-black px-36 py-2 text-white mr-3 cursor-pointer'> Set User </button>
+                    
                 <button className='bg-black px-36 py-2 text-white mr-3 cursor-pointer '> Clear User </button>
 
             </div>
