ca40bccae3ce03aa5eddbcc3ff38e5a6909aa126
nidhi <kumarinidhi0594@gmail.com>
2026-01-31 11:22:35 +0100

    Design Complete

--- project/src/projects/shortCircuit/ShortCircuit.jsx
+++ project/src/projects/shortCircuit/ShortCircuit.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 
 const ShortCircuit = () => {
-  return (
-    <div className=' mt-10 '>
-      <div>
-        <h1 className='font-bold text-4xl mb-10 flex items-center justify-center'> Welcome to the ShortCircuit Evaluation </h1>
+    return (
+        <div className=' mt-10 '>
+            <div className='font-bold text-4xl mb-10 flex items-center justify-center'>
+                <h1> Welcome to the ShortCircuit Evaluation !</h1>
+            </div>
 
-        <p className='mb-5'> You are logged in!</p>
+            <div className='ml-24'>
+                <p className='mb-5 text-xl'> You are logged in!</p>
+                <button className='bg-black px-36 py-2 text-white mr-3'> Toggle Login State </button>
+                <button className='bg-black px-36 py-2 text-white mr-3'> Set User </button>
+                <button className='bg-black px-36 py-2 text-white mr-3'> Clear User </button>
+
+            </div>
         </div>
-
-        <div> 
-          <button className='bg-black px-4 py-2 text-white mr-3'> Toggle Login State </button>
-          <button className='bg-black px-4 py-2 text-white mr-3'> Set User </button>
-          <button className='bg-black px-4 py-2 text-white mr-3'> Clear User </button>
-        
-      </div>
-    </div>
-  )
+    )
 }
 
 export default ShortCircuit
