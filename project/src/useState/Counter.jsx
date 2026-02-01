e6c782a14120762d2aed5a9ca133ef64bc52a57c
nidhi <kumarinidhi0594@gmail.com>
2026-02-01 11:58:50 +0100

    usestate Very first Example

--- project/src/useState/Counter.jsx
+++ project/src/useState/Counter.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react"
 
 const Counter = () => {
-    const [first, setfirst] = useState("")
+    const [count, setCount] = useState(0)
+
+const handleCount = () => {
+    setCount(count + 1)
+}
 
   return (
     <div className="flex items-center justify-center flex-col mt-10">
       <h1 className="font-bold text-4xl"> Usestate Hooks </h1>
       <br />
-      <p className="mb-4"> Count </p>
-      <button className="bg-black px-4 py-2 text-white"> Increment  </button>
+      <p className="mb-4 text-2xl"> Count : { count } </p>
+      <button onClick={handleCount}
+      className="bg-black px-4 py-2 text-white rounded cursor-pointer"> Increment  </button>
     </div>
   )
 }
