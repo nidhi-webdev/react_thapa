f397a27719cffce1e20553ca0cabed66fb260570
nidhi <kumarinidhi0594@gmail.com>
2026-02-01 12:40:29 +0100

    usestate Challenge

--- project/src/useState/Counter.jsx
+++ project/src/useState/Counter.jsx
@@ -3,19 +3,37 @@ import { useState } from "react"
 const Counter = () => {
     const [count, setCount] = useState(0)
 
-const handleCount = () => {
-    setCount(count + 1)
-}
+    const handleIncrement = () => {
+        setCount(count + 1)
+    }
 
-  return (
-    <div className="flex items-center justify-center flex-col mt-10">
-      <h1 className="font-bold text-4xl"> Usestate Hooks </h1>
-      <br />
-      <p className="mb-4 text-2xl"> Count : { count } </p>
-      <button onClick={handleCount}
-      className="bg-black px-4 py-2 text-white rounded cursor-pointer"> Increment  </button>
-    </div>
-  )
+     const handleDecrement = () => {
+        setCount(count - 1)
+    }
+
+     const handleReset = () => {
+        setCount(0)
+    }
+    
+    
+
+    return (
+        <div className="flex items-center justify-center flex-col mt-10">
+            <h1 className="font-bold text-4xl"> Usestate Hooks </h1>
+            <br />
+            <p className="mb-4 text-2xl"> Count : {count} </p>
+            <div className="mb-5"> Step: <input type="text" className="border-green-800 border-2 rounded" /> </div>
+            <div className="">
+                <button onClick={handleIncrement}
+                    className="bg-black px-6 py-3 text-white rounded cursor-pointer mr-5   "> Increment  </button>
+                <button onClick={handleDecrement}
+                    className="bg-black px-6 py-3 text-white rounded cursor-pointer mr-5"> Decrement  </button>
+                <button onClick={handleReset}
+                    className="bg-black px-6 py-3 text-white rounded cursor-pointer mr-5"> Reset  </button>
+            </div>
+
+        </div>
+    )
 }
 
 export default Counter
