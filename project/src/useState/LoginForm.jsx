2c18cb7ed36d1f137cae55b8ad6930903d2b0fbc
nidhi <kumarinidhi0594@gmail.com>
2026-02-04 21:00:31 +0100

    there is one way to submit the form

--- project/src/useState/LoginForm.jsx
+++ project/src/useState/LoginForm.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
+import { useState } from "react"
 
 const LoginForm = () => {
+    const [userName, setUserName] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(userName);
+        console.log(password);
+        setUserName("")
+        setPassword("")
+        
+    }
+
     return (
         <div className='flex justify-center items-center min-h-screen bg-linear-to-br from-red-100 to-red-300'>
-            <form className='bg-white shadow-2xl rounded-xl px-8 py-10 w-full max-w-sm'>
+            <form className='bg-white shadow-2xl rounded-xl px-8 py-10 w-full max-w-sm' onSubmit={handleSubmit}>
                 <h1 className='text-3xl font-bold mb-6 text-center'>Login Form</h1>
                 <div className='flex flex-col gap-6'>
                     <div>
                         <label className='block mb-1 font-medium'>Username</label>
                         <input type='text' name='username' required autoComplete='off'
+                            value={userName}
+                            onChange={(e) => setUserName(e.target.value)}
+
                             className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-red-200" />
                     </div>
                     <div>
                         <label className='block mb-1 font-medium'>Password</label>
                         <input type='password' name='password' required autoComplete='off'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                             className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-red-200" />
                     </div>
                 </div>
