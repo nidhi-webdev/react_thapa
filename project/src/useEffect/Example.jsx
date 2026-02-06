cbcc257834e9925b3cba43d84f7c303faf9b0e83
nidhi <kumarinidhi0594@gmail.com>
2026-02-06 13:49:24 +0100

    Examle 3 : Showing Time

--- project/src/useEffect/Example.jsx
+++ project/src/useEffect/Example.jsx
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from 'react'
 
 const Example = () => {
-    const [count, setCount] = useState(0)
+    const [date, setDate] = useState(0)
 
     useEffect(() => {
-        console.log("Count Value:", count);
+       const updatedDate = new Date();
+    setDate(updatedDate.toLocaleDateString())
 
-    }, [count])
+    }, [])
 
 
 
     return (
         <div className='p-10'>
-            <h1 className='font-bold mb-5'> UseEffect Hook </h1>
+            <h1 className='font-bold mb-5'> Date: {date} </h1>
 
-            <p className='mb-4'> Count: {count} </p>
-            <button onClick={() => setCount(count + 1)}
-            className='bg-sky-700 px-2 py-3 rounded text-white '> Increment </button>
+            
+          
 
 
         </div>
