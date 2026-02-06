8eab495da963d41d657e6db6ec62139080f57005
nidhi <kumarinidhi0594@gmail.com>
2026-02-06 13:50:54 +0100

    Examle 3 : Showing Time now

--- project/src/useEffect/Example.jsx
+++ project/src/useEffect/Example.jsx
@@ -4,8 +4,8 @@ const Example = () => {
     const [date, setDate] = useState(0)
 
     useEffect(() => {
-       const updatedDate = new Date();
-    setDate(updatedDate.toLocaleDateString())
+        const updatedDate = new Date();
+        setDate(updatedDate.toLocaleTimeString())
 
     }, [])
 
@@ -15,8 +15,8 @@ const Example = () => {
         <div className='p-10'>
             <h1 className='font-bold mb-5'> Date: {date} </h1>
 
-            
-          
+
+
 
 
         </div>
