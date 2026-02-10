09f2dfb08633c615b30d94c902d3dfd6c3b23bfd
nidhi <kumarinidhi0594@gmail.com>
2026-02-10 20:45:17 +0100

    Count

--- project/src/useEffect/UseEffectExample.jsx
+++ project/src/useEffect/UseEffectExample.jsx
@@ -4,21 +4,23 @@ import { useState } from "react"
 
 const UseEffectExample = () => {
     const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
 
-  return (
-    // <div></div>
-    <div className="flex justify-center items-center min-h-screen flex-col gap-y-4 bg-[#061935]">
+    return (
 
-      <h1 className="font-extrabold  text-white"> Use Effect Challenge </h1>
-      <p className="text-white"> Count: {count} </p>
+        <div className="flex justify-center items-center min-h-screen flex-col gap-y-4 bg-[#061935]">
 
-      <button className="bg-green-600 px-4 py-3 rounded text-white"> Increment </button>
+            <h1 className="font-medium text-white text-2xl"> Use Effect Challenge </h1>
+            <p className="text-white"> Count: {count} </p>
 
-      <p className="text-white"> Name: </p>
+            <button onClick={() => setCount(count + 1)}
+                className="bg-green-600 px-4 py-3 rounded text-white cursor-pointer"> Increment </button>
 
-      <input className="bg-white px-4 py-3 rounded"/>
-    </div>
-  )
+            <p className="text-white"> Name: {name}  </p>
+
+            <input className="bg-white px-4 py-3 rounded" />
+        </div>
+    )
 }
 
 export default UseEffectExample
