f0dfc17a6b5a9e7d6f015710d95d7ce0f6ddf76e
nidhi <kumarinidhi0594@gmail.com>
2026-02-11 18:29:30 +0100

    This is the corect way to fetch the data with useeffect formate

--- project/src/useEffect/HowNotToFetchApi.jsx
+++ project/src/useEffect/HowNotToFetchApi.jsx
@@ -1,22 +1,22 @@
 import { useEffect, useState } from "react"
 
 const HowNotToFetchApi = () => {
-const [apiData, setApiData] = useState([])
+    const [apiData, setApiData] = useState([])
 
 
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((res) => res.json())
-        .then((data) => {
-            setApiData(data)
-        })
-        .catch((err) => console.log(err))
+            .then((res) => res.json())
+            .then((data) => {
+                setApiData(data)
+            })
+            .catch((err) => console.log(err))
     }, [])
 
 
     return (
         <div>
-            <ul> data: 
+            <ul> data:
                 {apiData.map((curData) => {
                     return <li key={curData.id}> {curData.title} </li>
                 })}
