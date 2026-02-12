578b9a94c04cac54cd3d9edd5c9d5140623a449c
nidhi <kumarinidhi0594@gmail.com>
2026-02-12 18:55:59 +0100

    Correct design

--- project/src/useEffect/HowNotToFetchApi.jsx
+++ project/src/useEffect/HowNotToFetchApi.jsx
@@ -24,8 +24,8 @@ const HowNotToFetchApi = () => {
     return (
         <div className="min-h-screen flex flex-col items-center justify-start bg-[#f6f7fb] py-8">
             <h1 className="text-5xl font-extrabold text-black mb-8 text-center">Lets Catch Pokémon</h1>
-          
-          <div className="relative w-100 h-87.5 bg-white rounded-md shadow-xl flex items-center justify-center">
+
+            <div className="relative w-100 h-87 bg-white rounded-md shadow-xl flex flex-col items-center justify-center">
                 {/* Ellipse/oval at the top */}
                 <svg
                     className="absolute top-0 left-0"
@@ -38,10 +38,10 @@ const HowNotToFetchApi = () => {
                     <ellipse cx="200" cy="60" rx="190" ry="50" fill="#eafcf3" />
                 </svg>
                 <img className="relative z-10"
-                src= {apiData.sprites?.other?.dream_world?.front_default} alt="pokemon-img" />
-                <h1> {apiData.name} </h1>
-               
+                    src={apiData.sprites?.other?.dream_world?.front_default} alt="pokemon-img" />
+                <h1 className="text-5xl font-extrabold text-black text-center capitalize mt-4"> {apiData.name} </h1>
             </div>
+
         </div>
     )
 }
